7e6ad0cfa6790c9fa06f3570150eecd06371da21
HAZEM <73290269+Haazem@users.noreply.github.com>
2022-12-17 13:38:49 +0200

    Update Data Cleaning and Transformation.sql

--- Case Study #2: Pizza Runner/SQL Code/Data Cleaning and Transformation.sql
+++ Case Study #2: Pizza Runner/SQL Code/Data Cleaning and Transformation.sql
@@ -1,3 +1,4 @@
+USE [Pizza Runner];
 
 -- Clean exclusions & extras coulmns
 
@@ -36,8 +37,52 @@ WHERE cancellation = 'null' OR cancellation = ' ';
 
 ----------------------------------------------------------------------
 
+SELECT * FROM pizza_runner.runner_orders;
+-- Need to Rmove km From distance column
+
+-- find the rows needed to clean
+SELECT 
+       STUFF(distance, PATINDEX('%[km]%', distance), 2 , '') as distance_modified
+FROM pizza_runner.runner_orders
+WHERE distance IS NOT NULL AND 
+      distance LIKE '%[^0-9]';
+
+-- update the distance column
+UPDATE pizza_runner.runner_orders
+SET distance = STUFF(distance, PATINDEX('%[km]%', distance), 2 , '')
+WHERE distance IS NOT NULL AND 
+      distance LIKE '%[^0-9]';
+
+-- update datetype of distance column from string to number
+
+ALTER TABLE pizza_runner.runner_orders
+ALTER COLUMN distance NUMERIC(10,2);
+
+---------------------------------------------------------------------
+
+SELECT * FROM pizza_runner.runner_orders;
+
+-- find the target rows
+SELECT  STUFF(duration,PATINDEX('%[^0-9]%', duration), LEN(duration) , '') as duration_2,
+		duration
+FROM pizza_runner.runner_orders
+WHERE duration IS NOT NULL AND 
+      duration LIKE '%[a-z]%';
+
+--update duration column	 
+UPDATE pizza_runner.runner_orders
+SET duration = STUFF(duration,PATINDEX('%[^0-9]%', duration), LEN(duration) , '')
+WHERE duration IS NOT NULL AND 
+      duration LIKE '%[a-z]%';
+
+-- update duration column data type
+ALTER TABLE pizza_runner.runner_orders
+ALTER COLUMN duration INT;
+
+SELECT * FROM pizza_runner.runner_orders;
 
 
+----------------------------------------------------------------------
 
 
 
